960845a494b1454078724e0d55e4a58067cd333d
luongbangnguyen <luongbangvh@gmail.com>
2015-02-09 08:49:53 +0700

    change 9-2

---
+++ SpringAgulajs/src/main/java/com/java/spring/angular/unitest/MyUnitTest.java
@@ -0,0 +1,16 @@
+package com.java.spring.angular.unitest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.java.spring.angular.controller.UserController;
+import com.java.spring.angular.model.ModelAndView;
+
+public class MyUnitTest {
+
+    @Test
+    public void testUser() {
+    	UserController user = new UserController();
+    }
+}
